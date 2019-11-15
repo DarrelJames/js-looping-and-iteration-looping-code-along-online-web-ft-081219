878c32ce9ccd6e5f57170659a3ccd951caa4717a
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-14 18:40:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array) {
+  let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i];
+
+    newArr.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  return newArr;
+}
+
+function countDown(num) {
+  let i = num
+
+  while (i >= 0) {
+    console.log(i)
+
+    i--
+  }
+}
